a976c9cc8ce3aefa5bf4d1dcf9dd444d9bc47673
kantrax <diegogccantarell@gmail.com>
2022-06-07 13:28:22 -0500

    6 adding the root file 'index.js'

---
+++ index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require('./app/missionCommander');
+const Diego = new MissionCommander("Diego")
+const example2 = new MissionCommander("example2")
+const example3 = new MissionCommander("example3")
+console.log(Diego.name)
+console.log(example2.name)
+console.log(example3.name)
+
